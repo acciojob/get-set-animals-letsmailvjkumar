13a93248fbbbcee658262097bda422860c7181f0
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-07-24 23:22:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //complete this code
-class Animal {}
+class Animal {
 	constructor(species){
 		this._species=species;
 	}
@@ -7,22 +7,25 @@ class Animal {}
 		return this._species;
 	}
 	 makeSound(){
-		console.log("Animal sound: ");
+		console.log("Animal sound");
 	}
+}
 class Dog extends Animal {
 	bark(){
 		console.log("woof");
 	}
 	makeSound(){
+		super.makeSound();
 		this._bark();
 	}
 }
 	
 class Cat extends Animal {
 	purr(){
-		console.log("woof");
+		console.log("purr");
 	}
 	makeSound(){
+		super.makeSound();
 		this._purr();
 	}
 }
